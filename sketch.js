6de9ab1511adca8c082dfd674a2a7a4b64285e43
shreeya578 <71208095+shreeya578@users.noreply.github.com>
2020-10-18 13:01:04 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,23 +1,23 @@
-var car, wall, speed, weight, deformation;
+var bullet, wall, speed, weight, deformation;
 function setup() {
   createCanvas(1600,400);
   speed=random(55,80);
   weight=random(400,1500);
-  car=createSprite(200, 200, 50, 50);
-  car.velocityX=speed;
+  bullet=createSprite(200, 200, 50, 50);
+  bullet.velocityX=speed;
   wall=createSprite(1400,200,30,200);
 }
 
 function draw() {
   background(0);  
-  if(wall.x-car.x<(wall.width+car.width)/2){
-    car.velocityX=0;
+  if(wall.x-bullet.x<(wall.width+bullet.width)/2){
+    bullet.velocityX=0;
     deformation=(0.5*weight*speed*speed)/22500;
     if(deformation>180)
-      car.shapeColor="red";
+      bullet.shapeColor="red";
     else if (deformation<180&&deformation>80)
-      car.shapeColor="yellow";
-      else car.shapeColor="green";
+      bullet.shapeColor="yellow";
+      else bullet.shapeColor="green";
   }
 
 
